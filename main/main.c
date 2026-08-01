1998cde076c65fab6940e208824e0bee5015c0a4
Robin SHAMSNEJAD <rsham@free.fr>
2026-08-01 17:11:58 +0200

    Switch to multi-led

--- main/main.c
+++ main/main.c
@@ -1,104 +1,64 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-#include "led_strip.h"
-#include "sdkconfig.h"
 
-static const char *TAG = "example";
+#define LED_RED GPIO_NUM_6
+#define LED_GREEN GPIO_NUM_5
+#define LED_BLUE GPIO_NUM_4
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-
-static uint8_t s_led_state = 0;
-
-#ifdef CONFIG_BLINK_LED_STRIP
-
-static led_strip_handle_t led_strip;
-
-static void blink_led(void)
+typedef struct led_task_parameters_t
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
+    gpio_num_t led_gpio;
+    TickType_t blink_time;
+} led_task_parameters_t;
+
+static led_task_parameters_t red_led_gpio = {LED_RED, 2000};
+static led_task_parameters_t blue_led_gpio = {LED_BLUE, 1000};
+static led_task_parameters_t green_led_gpio = {LED_GREEN, 500};
+
+void led_task(void *pvParameter)
+{
+    gpio_num_t led_gpio = ((led_task_parameters_t *)pvParameter)->led_gpio;
+    TickType_t blink_time = ((led_task_parameters_t *)pvParameter)->blink_time;
+    uint8_t led_value = 0;
+    gpio_reset_pin(led_gpio);
+    gpio_set_direction(led_gpio, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(led_gpio, led_value);
+        led_value = !led_value;
+
+        printf("GPIO %d = %d\n", led_gpio, led_value);
+        vTaskDelay(blink_time / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
-static void configure_led(void)
+void app_main()
 {
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
-    };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
-}
+    xTaskCreate(
+        &led_task,      // task function
+        "red_led_task", // task name
+        2048,           // stack size in words
+        &red_led_gpio,  // pointer to parameters
+        5,              // priority
+        NULL);          // out pointer to task handle
 
-#elif CONFIG_BLINK_LED_GPIO
+    xTaskCreate(
+        &led_task,       // task function
+        "blue_led_task", // task name
+        2048,            // stack size in words
+        &blue_led_gpio,  // pointer to parameters
+        5,               // priority
+        NULL);           // out pointer to task handle
 
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#else
-#error "unsupported LED type"
-#endif
-
-void app_main(void)
-{
-
-    /* Configure the peripheral according to the LED type */
-    configure_led();
-
-    while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(
+        &led_task,        // task function
+        "green_led_task", // task name
+        2048,             // stack size in words
+        &green_led_gpio,  // pointer to parameters
+        5,                // priority
+        NULL);            // out pointer to task handle
 }
